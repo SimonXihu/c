7967fe6a0993fb1d7506c338f5b14c77525256e7
SimonXihu <895855913@qq.com>
2018-10-16 21:42:23 -0400

    sort file

---
+++ sort_algorithm/sort_algorithm.h
@@ -0,0 +1,13 @@
+#define ARRLENGTH 5
+
+//创建随机数组
+int create_arr(int **arr);
+
+//销毁随机数组
+int detroy_arr(int **arr);
+
+//冒泡
+int Bubble_sort(int *arr,int arr_length);
+
+//选择
+int Select_sort(int *arr,int arr_length);
